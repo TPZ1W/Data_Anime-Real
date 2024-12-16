c1ded3e8f5d635654398b98fcb5555f31109113a
YnLinh26305 <linhhtyl0@gmail.com>
2024-12-16 14:14:35 +0700

    Update data_clean_service.py

--- data_clean_service.py
+++ data_clean_service.py
@@ -3,7 +3,7 @@ class DataCleanService:
         pass
 
     def clean_data(self, data):
-        # tien hanh lam sach du lieu bang cach thay bang gia tri trung binh hoac gia tri xuat hien nhieu nhat
+        # lam sach du lieu bang cach thay bang gia tri trung binh hoac gia tri xuat hien nhieu nhat
         for col in data.columns:
             if data[col].dtype == 'object':
                 data[col].fillna(data[col].mode()[0], inplace=True)
